22de8a87e9c319272eb909644ef7a1d33c412211
Carter Fang <cfang@waabi.ai>
2024-11-19 18:28:53 +0000

    disable intrinsics aware

--- models/unet_adaptive_bins.py
+++ models/unet_adaptive_bins.py
@@ -27,9 +27,11 @@ class DecoderBN(nn.Module):
         super(DecoderBN, self).__init__()
         features = int(num_features)
 
-        self.conv2 = nn.Conv2d(bottleneck_features + 64, features, kernel_size=1, stride=1, padding=1)
-        self.intrinsics_mlp = nn.Linear(4, 64, bias=True)
-        self.intrinsics_norm = nn.LayerNorm(64)
+        self.conv2 = nn.Conv2d(bottleneck_features, features, kernel_size=1, stride=1, padding=1)
+        # self.intrinsics_mlp = nn.Sequential(nn.Linear(4, 16),
+        #                                     nn.LayerNorm(16),
+        #                                     nn.LeakyReLU(),
+        #                                     nn.Linear(16, 16))
 
         self.up1 = UpSampleBN(skip_input=features // 1 + 112 + 64, output_features=features // 2)
         self.up2 = UpSampleBN(skip_input=features // 2 + 40 + 24, output_features=features // 4)
@@ -44,10 +46,10 @@ class DecoderBN(nn.Module):
         x_block0, x_block1, x_block2, x_block3, x_block4 = features[4], features[5], features[6], features[8], features[
             11]
 
-        _, _, bottleneck_h, bottleneck_w = x_block4.size()
-        x_intri = intrinsics[:, [0, 0, 1, 1], [0, 2, 1, 2]].float() / 1e3
-        x_intri = self.intrinsics_norm(self.intrinsics_mlp(x_intri))[..., None, None].repeat(1, 1, bottleneck_h, bottleneck_w)
-        x_block4 = torch.cat([x_block4, x_intri], dim=1)
+        # _, _, bottleneck_h, bottleneck_w = x_block4.size()
+        # x_intri = intrinsics[:, [0, 0, 1, 1], [0, 2, 1, 2]].float() / 1e3
+        # x_intri = self.intrinsics_mlp(x_intri)[..., None, None].repeat(1, 1, bottleneck_h, bottleneck_w)
+        # x_block4 = torch.cat([x_block4, x_intri], dim=1)
         
         x_d0 = self.conv2(x_block4)
 
