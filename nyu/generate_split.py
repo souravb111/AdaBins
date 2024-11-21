849539d025f5bd3483ed883db1300e40c7e093a5
James Tu <jtu@waabi.ai>
2024-11-21 00:10:29 +0000

    final nyu splits

--- nyu/generate_split.py
+++ nyu/generate_split.py
@@ -1,5 +1,5 @@
 import os
-
+import math
 
 # FOCAL_LENGTH = 518.8579 # this is a model param for adabins
 DATA_DIR = "/mnt/remote/shared_data/datasets/nyu_depth_v2_sync/"
@@ -9,7 +9,7 @@ MINI_VAL_FACTOR = 10
 
 scenes = os.listdir(DATA_DIR)
 
-synced_rgbd: dict[str, [tuple[str, str]]] = {}
+synced_rgbd: dict[str, list[tuple[str, str]]] = {}
 
 for scene in scenes:
     scene_dir = os.path.join(DATA_DIR, scene)
@@ -21,28 +21,72 @@ for scene in scenes:
     rgb = [os.path.join(DATA_DIR, rgb_) for rgb_ in rgb]
     depth = [os.path.join(DATA_DIR, depth_) for depth_ in depth]
 
-    synced_rgbd[scene] = (list(zip(rgb, depth)))
+    synced_rgbd[scene] = [
+        [rgb_, depth_] for rgb_, depth_ in zip(rgb, depth)
+        if os.path.exists(rgb_) and os.path.exists(depth_)
+    ]
 
 val_scenes = scenes[1::5]
 train_scenes = [s for s in scenes if s not in val_scenes]
 
-val_rgbd = [rgbd for scene in val_scenes for rgbd in synced_rgbd[scene]]
-train_rgbd = [rgbd for scene in train_scenes for rgbd in synced_rgbd[scene]]
+target_train = 50000
+target_val = 10000
 
-print(f"Saving {len(train_rgbd)} images to train split.")
-print(f"Saving {len(val_rgbd)} images to val split.")
-print(f"Saving {len(val_rgbd) // MINI_VAL_FACTOR} images to mini val split.")
+train = []
+val = []
+
+# get even amount of data from all scenes
+# sample more uniformly in ordered frames
+total = 0
+num_from_scene: dict[str, int] = {s: 0 for s in train_scenes}
+while total < target_train:
+    for scene in train_scenes:
+        if num_from_scene[scene] == len(synced_rgbd[scene]):
+            continue
+        num_from_scene[scene] += 1
+        total += 1
+        if total >= target_train:
+            break
+
+for scene, num in num_from_scene.items():
+    if num == 0:
+        continue
+    stride = len(synced_rgbd[scene]) //  num
+    train.extend(synced_rgbd[scene][::stride])
+
+
+total = 0
+num_from_scene: dict[str, int] = {s: 0 for s in val_scenes}
+while total < target_val:
+    for scene in val_scenes:
+        if num_from_scene[scene] == len(synced_rgbd[scene]):
+            continue
+        num_from_scene[scene] += 1
+        total += 1
+        if total >= target_val:
+            break
+
+for scene, num in num_from_scene.items():
+    if num == 0:
+        continue
+    stride = len(synced_rgbd[scene]) //  num
+    val.extend(synced_rgbd[scene][::stride])
+
+
+print(f"Saving {len(train)} images to train split.")
+print(f"Saving {len(val)} images to val split.")
+print(f"Saving {len(val) // MINI_VAL_FACTOR} images to mini val split.")
 
 with open(f"{OUT_FILE}_val.csv", "w+") as f:
-    for rgbd in val_rgbd:
+    for rgbd in val:
         f.write(f"{rgbd[0]},{rgbd[1]}\n")
 
 with open(f"{OUT_FILE}_train.csv", "w+") as f:
-    for rgbd in train_rgbd:
+    for rgbd in train:
         f.write(f"{rgbd[0]},{rgbd[1]}\n")
 
 with open(f"{OUT_FILE}_mini_val.csv", "w+") as f:
-    for rgbd in val_rgbd[::MINI_VAL_FACTOR]:
+    for rgbd in val[::MINI_VAL_FACTOR]:
         f.write(f"{rgbd[0]},{rgbd[1]}\n")
 
 
