58ad2bc0527708c98ebccccfe0e126876a46a1af
James Tu <jtu@waabi.ai>
2024-11-24 22:20:32 +0000

    doesnt mtter

--- convert_torch_to_np.py
+++ convert_torch_to_np.py
@@ -1,20 +1,23 @@
+import numpy as np
 import os
 import torch
 from fast_np import save, load
 import sys
 
-#with open("nyu/nyu_depth_v2_train.csv") as f:
-#    pths = [x.split(",")[0].replace("nyu_depth_v2_sync", "nyu_sam_feats") for x in f.readlines()]   
+with open("nyu/nyu_depth_v2_train.csv") as f:
+    pths = [x.split(",")[0].replace("nyu_depth_v2_sync", "nyu_sam_feats") for x in f.readlines()]   
+with open("nyu/nyu_depth_v2_val.csv") as f:
+    pths2 = [x.split(",")[0].replace("nyu_depth_v2_sync", "nyu_sam_feats") for x in f.readlines()]   
 
-with open("/mnt/remote/shared_data/users/jtu/kitti_train.csv") as f:
-    pths = [x.split(",")[0].replace("kitti-depth", "kitti-depth-sam-feats") for x in f.readlines()]   
-with open("/mnt/remote/shared_data/users/jtu/kitti_val.csv") as f:
-    pths2 = [x.split(",")[0].replace("kitti-depth", "kitti-depth-sam-feats") for x in f.readlines()]   
+#with open("/mnt/remote/shared_data/users/jtu/kitti_train.csv") as f:
+#    pths = [x.split(",")[0].replace("kitti-depth", "kitti-depth-sam-feats") for x in f.readlines()]   
+#with open("/mnt/remote/shared_data/users/jtu/kitti_val.csv") as f:
+#    pths2 = [x.split(",")[0].replace("kitti-depth", "kitti-depth-sam-feats") for x in f.readlines()]   
 
 pths = pths + pths2
 
 STRIDE = 8
-START = 0 + int(sys.argv[1])
+START = 5600 + int(sys.argv[1])
 
 idcs = list(range(len(pths)))
 
@@ -24,13 +27,21 @@ for i in idcs[START::STRIDE]:
     
     if not os.path.exists(pth):
         continue
-    feats = torch.load(pth).numpy()
-    #dest = pth.replace("nyu_sam_feats", "nyu_sam_feats_np")
-    dest = pth.replace("kitti-depth-sam-feats", "kitti-depth-sam-feats-np").replace(".png", ".npy")
+    feats = torch.load(pth)
+    feats = torch.nn.functional.interpolate(feats, scale_factor=0.25).numpy()
+    dest = pth.replace("nyu_sam_feats", "nyu_sam_feats_downsample").replace(".png", ".npy")
+    #dest = pth.replace("kitti-depth-sam-feats", "kitti-depth-sam-feats-np").replace(".png", ".npy")
     ddir = os.path.dirname(dest)
     if not os.path.isdir(ddir):
-        os.makedirs(ddir)
-    save(dest, feats)
+        try:
+            os.makedirs(ddir)
+        except FileExistsError:
+            print("skip makefile, exists")
     print(f"Saving to {dest}")
+    try:
+        save(dest, feats)
+    except Exception:
+        save(dest, feats)
+
 
 
